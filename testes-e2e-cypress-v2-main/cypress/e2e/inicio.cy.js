226bbe719981d510c1f00786b7bb2a889617c06b
Eron Moraes <eron.moraes7@gmail.com>
2024-01-12 19:52:30 -0300

    tst

--- testes-e2e-cypress-v2-main/cypress/e2e/inicio.cy.js
+++ testes-e2e-cypress-v2-main/cypress/e2e/inicio.cy.js
@@ -5,6 +5,6 @@ describe('Página inicial', () => {
   it('Deve renderizar o h1 com o texto correto', () => {
     cy.getByData('titulo-principal').contains(
       'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
-    );
-  });
-});
+    )
+  })
+})
